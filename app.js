2732648341b512f703542c1fc712e3ffe34c24d6
Jaro111 <jarokark@protonmail.com>
2024-05-16 09:31:36 +0100

    change image

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const click = document.getElementById("click");
 const body = document.getElementById("body");
 
 click.addEventListener("click", () => {
-  love.textContent = "KOCHAM CIĘ ŚLICZNOTKO";
-  love2.textContent = "MNIEJ NIŻ JUTRO I BARDZIEJ NIŻ WCZORAJ";
-  body.style.backgroundImage = "url('wallpaperflare.com_wallpaper.jpg')";
+  love2.textContent = "KOCHAM CIĘ MÓJ PINGWINKU";
+  body.style.backgroundImage = "url('1.5-13vsydu.jpg')";
+  body.style.backgroundRepeat = "none";
   click.style.display = "none";
 });
